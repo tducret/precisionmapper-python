8c8e45bfaa3dd026bff27fe955cb1d9411f99d44
tducret <thibault.ducret@gmail.com>
2018-08-02 05:33:09 +0000

    Generated code by SwaggerHub

---
+++ python-flask/swagger_server/controllers/user_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.survey import Survey  # noqa: E501
+from swagger_server import util
+
+
+def list_surveys():  # noqa: E501
+    """list the surveys available
+
+    List the surveys available # noqa: E501
+
+
+    :rtype: List[Survey]
+    """
+    return 'do some magic!'
---
+++ python-flask/swagger_server/test/test_user_controller.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.survey import Survey  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestUserController(BaseTestCase):
+    """UserController integration test stubs"""
+
+    def test_list_surveys(self):
+        """Test case for list_surveys
+
+        list the surveys available
+        """
+        response = self.client.open(
+            '/tducret/PrecisionMapper/1.0.0/surveys',
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
